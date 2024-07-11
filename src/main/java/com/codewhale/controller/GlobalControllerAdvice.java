7e58a5b370dbe5707a7f61f2d1e5b51951fc1318
brownieSea <atticroom01@gmail.com>
2024-07-11 14:18:19 +0900

    .

--- src/main/java/com/codewhale/controller/GlobalControllerAdvice.java
+++ src/main/java/com/codewhale/controller/GlobalControllerAdvice.java
@@ -12,8 +12,7 @@ public class GlobalControllerAdvice {
     public String sid(HttpSession session) {
         // 세션에서 sessionId 속성을 가져와서 sid 변수에 저장
         String sid = (String) session.getAttribute("sessionId");
-        // sid가 null일 경우에는 공백 문자열 반환 (또는 다른 처리 방법)
-        return sid;
+         return sid;
     }
 
 }
